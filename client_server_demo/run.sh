a2e291d9a201442521640148c207e6624c0386a8
gsmaster8 <dengshijie@hnu.edu.cn>
2021-03-30 11:21:51 +0800

    add file

---
+++ client_server_demo/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+echo "Run Client_Server Demo ..."
+
+./server&
+./client
+
+#####################################
+#   chmod +x ./run.sh && ./run.sh   #
+#####################################
